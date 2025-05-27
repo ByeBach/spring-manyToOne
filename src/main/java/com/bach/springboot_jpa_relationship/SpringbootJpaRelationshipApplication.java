7a06694326069e9dbcb5d0f3dda072d114ab818e
bach <byebachgm@gmail.com>
2025-05-27 10:44:39 -0300

    Ejemplos many to one implementados

--- src/main/java/com/bach/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/bach/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,5 +1,7 @@
 package com.bach.springboot_jpa_relationship;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,19 +25,33 @@ public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		//manyToOneCreateClient();
+		manyToOneFindById();
 	}
 
-	public void manyToOne(){
+	public void manyToOneCreateClient(){
 		Client client = new Client("jhon", "Connor");
 		clientRepository.save(client);
 
 		Invoice invoice = new Invoice("compras para setUp", 1000L );
 		invoice.setClient(client);
 
-		Invoice invoiceDB = invoiceRepository.save(invoice);
-		System.out.println(invoiceDB);
+		//Invoice invoiceDB = invoiceRepository.save(invoice);
+		//System.out.println(invoiceDB);
 
+	}
+	public void manyToOneFindById(){
+		//buscamos al cliente y asignamos la factura
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if(optionalClient.isPresent()){
+			Client client = optionalClient.orElseThrow();
+			Invoice invoice = new Invoice("compras para setUp", 1000L );
+			invoice.setClient(client);
+
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println(invoiceDB);
+		}
 	}
 
 
